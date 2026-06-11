3df31acddfd751f3f12665e1f42c8598d923b0c3
AntonMan12 <anton356is@gmail.com>
2026-06-11 11:39:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ components.html(
             if (marker) {{
                 marker.scrollIntoView({{ block: 'start', behavior: 'auto' }});
             }}
-        }}, 100);
+        }}, 10);
     </script>
     """,
     height=1
